d8af4d7f4adbec2834f0743361234febfc6fdc5b
specificlanguage <specificlanguage@gmail.com>
2023-12-08 14:36:29 -0500

    chore: Setup libraries

---
+++ src/environments/environment.sample.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+  },
+  production: false,
+  apiUrl: "http://localhost:8000"
+};
